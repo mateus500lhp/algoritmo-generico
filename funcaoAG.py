3439982ed87415911883985481c7a74d5f38c230
Mateus Oliveira <62905966+mateus500lhp@users.noreply.github.com>
2022-04-19 15:25:52 -0300

    Add files via upload

--- funcaoAG.py
+++ funcaoAG.py
@@ -1,9 +1,7 @@
 # coding: utf-8
 import math
 import random
-import matplotlib as mpl
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
 def func_obj(x):
@@ -87,6 +85,45 @@ def torneio(npop, fit):
         cruzamento(pop, npop ,pop_intermediaria, vencedor1, vencedor2, dimensao, precisao)
 
 
+def selecao_roleta(pop, npop, fit, pop_intermediaria, roleta):
+    fitaux = []
+    roleta = []
+    while(len(pop_intermediaria) < npop):
+        for i in fit:
+            fitaux.append(1/i)
+
+        sumfit = sum(fitaux)
+
+        for i in fitaux:
+            roleta.append(i/sumfit)
+
+        sortp1 = random.random()
+        sortp2 = random.random()
+        # print('Sorte valor p1: {}'.format(sortp1))
+        # print('Sorte valor p2: {}'.format(sortp2))
+        vencedor1 = 0
+        vencedor2 = 0
+        somatorio_roleta = 0
+
+        for i in roleta:
+            if(somatorio_roleta < sortp1):
+                somatorio_roleta += i
+                vencedor1 = roleta.index(i)
+        #print('Vencedor 1: {}'.format(vencedor1))
+        
+        somatorio_roleta = 0
+        for i in roleta:
+            if(somatorio_roleta < sortp2):
+                somatorio_roleta += i
+                vencedor2 = roleta.index(i)
+        #print('Vencedor 2: {}'.format(vencedor2))
+
+        # print(vencedor1)
+        # print(vencedor2)
+        #print('Soma: {}'.format(sum(roleta)))
+
+        cruzamento(pop, npop, pop_intermediaria, vencedor1, vencedor2, dimensao, precisao)
+
 def cruzamento(pop, npop, pop_intermediaria, vencedor1, vencedor2, dimensao, precisao):
     alelo = random.randrange(1,dimensao*precisao-1)
 
@@ -116,7 +153,6 @@ def mutacao(pop_intermediaria, npop, pm, dimensao, precisao):
 def elitismo(pop, npop, pop_intermediaria, fit, dimensao, precisao):
     fit_min = min(fit)
     thebest.append(fit_min)
-    print(fit_min)
     posicao = fit.index(fit_min)
     index = random.randrange(0, npop-1)
     #print(index)
@@ -128,14 +164,15 @@ x_max = 2
 dimensao = 2
 precisao = 6
 
-npop = 100 #tamanho da população
-nger = 20 #numero de gerações
+npop = 50 #tamanho da população
+nger = 4 #numero de gerações
 nelite = 2
 pop = []
 pop_intermediaria = []
 
 fit = []
 thebest = []
+roleta = []
 
 pc = 1 #probabilidade de cruzamento
 pm = 0.1 #probabilidade de mutação
@@ -145,36 +182,18 @@ g=0
 while g < nger:
     for i in pop:
         avaliacao(i)
-    torneio(npop, fit)
+   # torneio(npop, fit)
+    selecao_roleta(pop, npop, fit, pop_intermediaria, roleta)
     elitismo(pop, npop, pop_intermediaria, fit, dimensao, precisao)
     pop = pop_intermediaria
     pop_intermediaria = []
     fit = []
     g += 1
-
-    # for i in pop:
-    #     print(i)
+    for i in pop:
+        print(i)
     print('------------------------------------')
 
-
-
-def plot(thebest):
-
-    fig = plt.figure()
-    ax =  plt.axes(projection='3d')
-
-    # Make data.
-    X = np.arange(-2, 2, 0.25)
-    Y = np.arange(-2, 2, 0.25)
-
-    Z = thebest
-
-    X, Y = np.meshgrid(X, Y)
-    Z = np.array(Z)
-
-    # Plot the surface.
-    surf = ax.plot_surface(X, Y, Z)
-
-    plt.show()
-
-plot(thebest)
+# x = np.arange(1, nger+1,1)
+# plt.plot(x, thebest, 'k--')
+# plt.plot(x, thebest, 'go')
+# plt.show()
